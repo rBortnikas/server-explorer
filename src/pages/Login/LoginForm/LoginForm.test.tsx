552c82be8cb6c07cd9e0db566c6210e2bcd1e8e3
rBortnikas <bortnro@gmail.com>
2020-03-27 21:39:31 +0200

    add test

--- src/pages/Login/LoginForm/LoginForm.test.tsx
+++ src/pages/Login/LoginForm/LoginForm.test.tsx
@@ -3,7 +3,7 @@ import { render, fireEvent, wait } from "@testing-library/react";
 import { LoginForm } from "./LoginForm";
 
 describe("LoginForm", () => {
-  test("submits with both inputs filled in", async () => {
+  test("submits with both inputs filled in and returns credentials", async () => {
     const onSubmit = jest.fn();
     const { getByText, getByTestId } = render(
       <LoginForm onSubmit={onSubmit} />
@@ -32,5 +32,31 @@ describe("LoginForm", () => {
       username: mockUsername,
       password: mockPassword
     });
+  });
+
+  describe("error handling", async () => {});
+  test("does not submit with a missing input and displays the correct error", async () => {
+    const onSubmit = jest.fn();
+    const { queryByText, getByText, getByTestId } = render(
+      <LoginForm onSubmit={onSubmit} />
+    );
+
+    const usernameInput = getByTestId("username-input");
+    const loginButton = getByText("Log In");
+    const mockUsername = "mockUsername";
+
+    await wait(() => {
+      fireEvent.change(usernameInput, {
+        target: { value: mockUsername }
+      });
+      fireEvent.click(loginButton);
+    });
+
+    const passwordErrorLabel = getByText("Password cannot be blank");
+    const usernameErrorLabel = queryByText("Username cannot be blank");
+
+    expect(onSubmit).toHaveBeenCalledTimes(0);
+    expect(passwordErrorLabel).toBeTruthy();
+    expect(usernameErrorLabel).toBeNull();
   });
 });
