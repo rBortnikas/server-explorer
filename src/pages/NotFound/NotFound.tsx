8fe4aebdc30c609f4f5ffd282d875ace66326f2a
rBortnikas <bortnro@gmail.com>
2020-03-29 17:21:54 +0300

    fix NotFound page

--- src/pages/NotFound/NotFound.tsx
+++ src/pages/NotFound/NotFound.tsx
@@ -1,5 +1,27 @@
 import React from "react";
+import styled from "styled-components";
+
+import { Colors } from "src/style/colors";
 
 export function NotFound() {
-  return <div>NotFound page</div>;
+  return (
+    <Wrapper>
+      <h1>Page not found.</h1>
+    </Wrapper>
+  );
 }
+
+const Wrapper = styled.div`
+  text-align: center;
+  flex-grow: 1;
+  background-color: ${Colors.almostBlack};
+  width: 100%;
+  box-sizing: border-box;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  @media (min-width: 800px) {
+    width: 800px;
+  }
+`;
